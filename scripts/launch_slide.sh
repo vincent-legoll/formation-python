5f0e043bc812cac6f95a4acfb841f12f830257f6
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2015-11-22 19:15:16 +0100

    bash script to convert notebook to slideshow and run corresponding server

---
+++ scripts/launch_slide.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+case $1 in
+  "0")
+      jupyter-nbconvert 00-InitPython-generalites.ipynb --to slides --post serve --ServePostProcessor.port=8000
+      ;;
+  "1")
+      jupyter-nbconvert 01-InitPython-langage-1.ipynb --to slides --post serve --ServePostProcessor.port=8001
+      ;;
+  "2")
+      jupyter-nbconvert 01-InitPython-langage-2.ipynb --to slides --post serve --ServePostProcessor.port=8002
+      ;;
+  esac
