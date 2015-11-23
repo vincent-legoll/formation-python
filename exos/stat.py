d3d76956d716c0d8b52a9858e145d6b0b5ecca5c
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2015-11-23 01:37:26 +0100

    Add 03-InitPython-langage file + exos

---
+++ exos/stat.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def stat(*args):
+    # votre fonction
+    return sum(args), min(args), max(args)
+
+stat(1, 4, 6, 9)
---
+++ exos/ttc.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def ttc(liste):
+    return sum(liste) * 1.186
+
+liste_prix_ht = [12, 56.20, 19.99, 0.40, 100]
+print "%.02f" % ttc(liste_prix_ht)
