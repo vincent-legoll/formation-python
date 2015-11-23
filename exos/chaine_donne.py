03e8c7b93bd370c2247b60c686d503e670dd688d
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-11-23 18:17:52 +0100

    pep8 style fix (spaces around operators) join() takes an iterator and does not mutate its content: no need to make a list

--- exos/chaine_donne.py
+++ exos/chaine_donne.py
@@ -2,10 +2,10 @@
 
 chaine_donnee = "azertyuioppoiuytreza"
 
-milieu = len(chaine_donnee)/2
+milieu = len(chaine_donnee) / 2
 print chaine_donnee[:milieu] + ',' + chaine_donnee[milieu:]
 
-intervale = len(chaine_donnee)/3
-print chaine_donnee[:intervale] + ':' + chaine_donnee[intervale:2*intervale+1] + ':' + chaine_donnee[2*intervale+1:]
+intervale = len(chaine_donnee) / 3
+print chaine_donnee[:intervale] + ':' + chaine_donnee[intervale:2 * intervale + 1] + ':' + chaine_donnee[2 * intervale + 1:]
 
-print '|'.join(list(chaine_donnee))
+print '|'.join(chaine_donnee)
