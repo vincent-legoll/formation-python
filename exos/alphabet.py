821a6e76a7ff6323ce58199170b295de9d9cae6c
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-11-23 18:18:26 +0100

    pep8

--- exos/alphabet.py
+++ exos/alphabet.py
@@ -2,6 +2,6 @@
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-print alphabet[:len(alphabet)/2]
-print alphabet[len(alphabet)/2:]
+print alphabet[:len(alphabet) / 2]
+print alphabet[len(alphabet) / 2:]
 print alphabet[::2]
