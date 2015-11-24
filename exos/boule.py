f8ab780b966f27139576679efbaefb8f0928850a
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2015-11-24 13:35:30 +0100

    correction in correction

--- exos/boule.py
+++ exos/boule.py
@@ -3,4 +3,4 @@
 def boule_or(*args, **kwargs):
     kwargs['couleur'] = "or"  # seule instruction qui fait une hypothèse sur la fonction boule() 
     return boule(*args, **kwargs)
-boule_or(1, 1)
+boule_or(1, 2, rendu='brillant')
