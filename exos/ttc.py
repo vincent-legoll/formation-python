17da94ae79b09028f52f7585c875e61faa1196fb
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2015-11-24 13:34:18 +0100

    TVA a 20% ;-)

--- exos/ttc.py
+++ exos/ttc.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def ttc(liste):
-    return sum(liste) * 1.186
+    return sum(liste) * 1.2
 
 liste_prix_ht = [12, 56.20, 19.99, 0.40, 100]
 print "%.02f" % ttc(liste_prix_ht)
