d96bca7085ebca30f3ef4c1cbbf25b1ae3e5307e
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-11-28 12:20:13 +0100

    More explanations: reverse slicing

--- exos/reverse.py
+++ exos/reverse.py
@@ -12,10 +12,18 @@ print L[::-1]
 # La version explicite :
 print L[-1:-len(l)-1:-1]
 
-# Explications:
+# Explications :
 # On commence à la dernière place
 # On s'arrête à la première, exprimée en indices négatifs
 # Le pas est de -1 => à l'envers
+
+# Ne pas oublier que les indices vont:
+# dans le sens normal : de 0 à 5
+# dans le sens contraire : de -1 à -6
+
+# Ne pas oublier que les bornes d'un slice qui prend tout les éléments vont:
+# dans le sens normal : de 0 à 6
+# dans le sens contraire : de -1 à -7
 
 # La solution "optimale" : elle utilise un iterateur et donc
 # ne consomme aucune ressource tant qu'on ne l'utilise pas 
