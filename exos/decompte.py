ffab4333ac244131901bf1afb37e0c6306dd5167
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-11-28 20:02:40 +0100

    time.sleep() vs notebook warning

--- exos/decompte.py
+++ exos/decompte.py
@@ -7,6 +7,8 @@ secs = 10
 while secs >= 0:
     print str(secs)
     secs -= 1
+    # La fonction time.sleep() ne fonctionne pas correctement
+    # quand elle est utilisée dans un notebook.
     time.sleep(1)
 
 print 'Bonne année !'
