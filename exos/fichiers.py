a8d332303293ad90ad92732845c1910fe69d73de
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-11-28 18:00:32 +0100

    clean file exercice up

--- exos/fichiers.py
+++ exos/fichiers.py
@@ -4,17 +4,22 @@ mystere = ["Gur Mra bs Clguba, ol Gvz Crgref\n\n",
            "Ornhgvshy vf orggre guna htyl.\n",
            "Rkcyvpvg vf orggre guna vzcyvpvg.\n"]
 
+# On écrit ces chaînes de caractères dans le fichier "coded.txt"
 f = open('coded.txt', mode='w')
 f.writelines(mystere)
 f.close()
+
+# On lit le contenu du fichier "coded.txt" que l'on vient de créer
 f = open('coded.txt', mode='r')
 coded = f.read()
 f.close()
-f = open('decoded.txt', mode='w')
+
+# On décode le message mystérieux
 decoded = coded.decode('rot13')
+
+# On écrit le message décodé dans un autre fichier
+f = open('decoded.txt', mode='w')
 f.write(decoded)
 f.close()
 
-# Pour les impatients :
-
-print ''.join(mystere).decode('rot13')
+# Vous pouvez ensuite aller consulter le message décodé dans le fichier "decoded.txt"
