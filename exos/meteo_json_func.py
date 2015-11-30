f9bb4ddd0f775e5cbffac4a37d66006a617c323e
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2015-11-30 17:11:00 +0100

    meteo_json_func: User prompt is more complete and robust

--- exos/meteo_json_func.py
+++ exos/meteo_json_func.py
@@ -12,18 +12,26 @@ import matplotlib.image as mpimg
 import numpy as np
 
 
-def get_city(city):
-    """Return a meteo json dictionary corresponding to city"""
+def get_city():
+    """From user input, return a meteo json dictionary corresponding to city"""
 
-    jsonfile_url = "http://www.prevision-meteo.ch/services/json/" + city
+    while True:  # Infinite loop to handle city name input
+        city_name = raw_input("Entrer la ville :\n")
+        jsonfile_url = "http://www.prevision-meteo.ch/services/json/"\
+                       + city_name
+        try:
+            f = urllib2.urlopen(jsonfile_url)  # open url
+            return json.load(f)
+        except ValueError:  # Intercept json load error
+            print "{} n'existe pas dans la base. Essayer un autre nom."\
+                  .format(city_name)
 
-    f = urllib2.urlopen(jsonfile_url)  # open url
-    return json.load(f)  # Read JSON file
 
+def plot_day_tempe(city_json, day_key):
+    """Plot Temperature vs hour from a json dictionary for a given day_key"""
 
-def plot_day_tempe(day):
-    """Plot Temperature vs hour from a day dictionary"""
-
+    city_name = city_json['city_info']['name']
+    day = city_json[day_key]
     day_hd = day['hourly_data']  # point to hourly data
 
     # Get tempe = [[h1, T1], [h2, T2], ...] list
@@ -41,14 +49,14 @@ def plot_day_tempe(day):
 
     # Plot T = T(hour)
     fig = plt.figure()  # initialise figure
-    title = "{} {}".format(day['day_long'], day['date'])
+    title = u"{} {} à {}".format(day['day_long'], day['date'], city_name)
     fig.suptitle(title, fontsize=14, fontweight='bold')
 
     ax = fig.add_subplot(111)  # initialise a plot area
     fig.subplots_adjust(top=0.85)
-    ax.set_title('Day temperature')
-    ax.set_xlabel('Time [h]')
-    ax.set_ylabel('Temperature [deg. C]')
+    ax.set_title(u'Evolution horaire')
+    ax.set_xlabel(u'Temps [h]')
+    ax.set_ylabel(u'Température [deg. C]')
 
     ax.plot(t[0], t[1])  # plot t[1] (tempe) as a function of t[0] (hour)
 
@@ -64,21 +72,25 @@ def plot_day_tempe(day):
 
 
 def get_day(city_json):
-    """From user input, return the key for day in json dictonary"""
+    """From user input, return the day key in json dictonary"""
 
     days = {day: data['day_long'] for day, data in city_json.iteritems()
             if day[:8] == "fcst_day"}  # Create {'fcst_day_#': week-day}
-    question = "Choix du jour :\n"
+    question = "Choisir le jour :\n"
     days_list = []  # Build ['fcst_day_#', week-day] sorted list
     # This i-loop is required because "days" is not sorted:
     for i in range(5):
         key = "fcst_day_{}".format(i)
         days_list.append([key, days[key]])
         question = question + "{}) {}\n".format(i, days[key])
-    choice = int(raw_input(question))
-    return days_list[choice][0]
+    while True:
+        try:
+            choice = int(raw_input(question))  # Prompt user for day index
+            return days_list[choice][0]  # Return 'fcst_day_#'
+        except:
+            print "Entrée non valide."
 
 
-city_json = get_city('Strasbourg')  # get json dict from city name
-day = get_day(city_json)  # get day key from user input
-plot_day_tempe(city_json[day])  # plot day temperature evolution
+city_json = get_city()  # get json dict from user input
+day_key = get_day(city_json)  # get day key from user input
+plot_day_tempe(city_json, day_key)  # plot day temperature evolution
