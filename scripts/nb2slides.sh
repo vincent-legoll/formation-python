7d92dc666b168dca9c5a88cd51e2bc30462da592
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-12-15 13:37:29 +0100

    Remove obsolete file

--- scripts/nb2slides.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-jupyter-nbconvert 00-InitPython-generalites.ipynb --to slides
-jupyter-nbconvert 01-InitPython-langage.ipynb --to slides
-jupyter-nbconvert 02-InitPython-langage.ipynb --to slides
